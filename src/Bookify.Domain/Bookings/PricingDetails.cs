93508091cc13244581f844e8c2a9c6faa429601b
Brayan Hernández Cortés <brayannhc07@gmail.com>
2023-10-21 15:05:50 -0600

    Domain Layer Section completed

---
+++ src/Bookify.Domain/Bookings/PricingDetails.cs
@@ -0,0 +1,9 @@
+﻿using Bookify.Domain.Apartments;
+
+namespace Bookify.Domain.Bookings;
+
+public record PricingDetails(
+    Money PriceForPerdiod,
+    Money CleaningFee,
+    Money AmenitiesUpCharge,
+    Money TotalPrice);
