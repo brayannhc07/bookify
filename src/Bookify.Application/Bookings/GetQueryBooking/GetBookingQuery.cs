bc9d3e1855ffcfc53c785cbf4315bcb2c4bffa50
Brayan Hernández Cortés <brayannhc07@gmail.com>
2023-11-04 13:21:05 -0600

    Application Layer Section Completed

---
+++ src/Bookify.Application/Bookings/GetQueryBooking/GetBookingQuery.cs
@@ -0,0 +1,5 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+
+namespace Bookify.Application.Bookings.GetQueryBooking;
+
+public record GetBookingQuery(Guid BookingId) : IQuery<BookingResponse>;
